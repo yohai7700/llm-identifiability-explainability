6c85e3240801e3c65f58850cac53fd749bdc718d
YohaiM <yohai7700@gmail.com>
2024-10-14 00:04:46 +0300

    small fix

--- testing/dataset.py
+++ testing/dataset.py
@@ -3,7 +3,7 @@ from datasets import load_dataset, DatasetDict
 from args import get_args
 
 YOHAI_CACHE_DIR = "/home/sharifm/teaching/tml-0368-4075/2024-spring/students/yohaimazuz/.cache"
-cache_dir = YOHAI_CACHE_DIR if get_args().cache_user is "yohai" else None
+cache_dir = YOHAI_CACHE_DIR if get_args().cache_user == "yohai" else None
 
 imdb_dataset = load_dataset("shawhin/imdb-truncated", cache_dir=cache_dir)
 yelp_dataset = load_dataset("Yelp/yelp_review_full", cache_dir=cache_dir)
