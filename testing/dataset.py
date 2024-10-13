f274fa1f073bf6561614c6212a9a050d063ada8f
YohaiM <yohai7700@gmail.com>
2024-10-14 00:12:32 +0300

    fix

--- testing/dataset.py
+++ testing/dataset.py
@@ -5,6 +5,8 @@ from args import get_args
 YOHAI_CACHE_DIR = "/home/sharifm/teaching/tml-0368-4075/2024-spring/students/yohaimazuz/.cache"
 cache_dir = YOHAI_CACHE_DIR if get_args().cache_user == "yohai" else None
 
+print(f"Using cache directory: {cache_dir}")
+
 imdb_dataset = load_dataset("shawhin/imdb-truncated", cache_dir=cache_dir)
 yelp_dataset = load_dataset("Yelp/yelp_review_full", cache_dir=cache_dir)
 
