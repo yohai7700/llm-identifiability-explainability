40b3e9b34b234ff402d8b03e05c41e744985bdf2
YohaiM <yohai7700@gmail.com>
2024-10-26 15:33:22 +0300

    test debug

--- models/classification_model.py
+++ models/classification_model.py
@@ -7,6 +7,8 @@ model_name = 'distilbert-base-uncased'
 id2label = {0: 'Negative', 1: 'Positive'}
 label2id = {v: k for k, v in id2label.items()}
 
+print('defining model with cache dir:', get_args().cache_dir)
+
 model = AutoModelForSequenceClassification.from_pretrained(
     model_name, 
     num_labels=2, 
