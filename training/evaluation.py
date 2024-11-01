4014135a839ae9d560eca19546f2f287912dd056
YohaiM <yohai7700@gmail.com>
2024-11-01 12:13:55 +0200

    Add F1 Score and Confusion Matrix to Evaluation Metrics

--- training/evaluation.py
+++ training/evaluation.py
@@ -1,11 +1,16 @@
 import evaluate
 import numpy as np
 
-accuracy = evaluate.load("accuracy")
+accuracy_metric = evaluate.load("accuracy")
+f1_metric = evaluate.load("f1")
+confusion_matrix_metric = evaluate.load("confusion_matrix")
 
 def compute_metrics(examples):
     predictions, labels = examples
     predictions = np.argmax(predictions, axis=1)
 
-    accuracy_value = accuracy.compute(predictions=predictions, references=labels)
-    return { 'accuracy': accuracy_value }
+    accuracy = accuracy_metric.compute(predictions=predictions, references=labels)
+    f1_score = f1_metric.compute(predictions=predictions, references=labels)
+    confusion_matrix = confusion_matrix_metric.compute(predictions=predictions, references=labels)
+    confusion_matrix['confusion_matrix'] = confusion_matrix['confusion_matrix'].tolist()
+    return { 'accuracy': accuracy, 'f1_score': f1_score, 'confusion_matrix': confusion_matrix }
