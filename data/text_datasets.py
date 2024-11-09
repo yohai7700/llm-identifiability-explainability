6d7cef0bb219a565307622f860ef9f1cfffbc0f8
YohaiM <yohai7700@gmail.com>
2024-11-09 17:56:14 +0200

    revert index shuffling

--- data/text_datasets.py
+++ data/text_datasets.py
@@ -1,7 +1,5 @@
-import torch
-from torch.utils.data import Subset
-
 from datasets import load_dataset
+from torch.utils.data import Subset
 
 from args import get_args
 
@@ -13,10 +11,7 @@ def load_text_datasets():
     elif dataset_type == "yelp":
         dataset = load_dataset("Yelp/yelp_review_full", cache_dir=get_args().cache_dir)
         train_dataset, eval_dataset = dataset['train'], dataset['test']
-    
-    training_indices = torch.randperm(len(train_dataset)).tolist()[:get_args().training_subset_size]
-    eval_indices = torch.randperm(len(eval_dataset)).tolist()[:get_args().eval_subset_size]
-    
-    train_dataset = Subset(train_dataset, training_indices)
-    eval_dataset = Subset(eval_dataset, eval_indices)
+        
+    train_dataset = Subset(train_dataset, range(get_args().training_subset_size))
+    eval_dataset = Subset(eval_dataset, range(get_args().eval_subset_size))
     return train_dataset, eval_dataset
